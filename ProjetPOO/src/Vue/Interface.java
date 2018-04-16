1d05f2e57088928bd9e48c607d64ed9c8347b348
MartinenghiJJ <38318267+MartinenghiJJ@users.noreply.github.com>
2018-04-16 17:45:44 +0200

    Add files via upload

---
+++ ProjetPOO/src/Vue/Interface.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+/**
+ *
+ * @author jjmar
+ */
+// Imports necessaire
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Modele.*;
+
+
+public class Interface extends JFrame implements ActionListener, ItemListener{
+    
+    
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ ProjetPOO/src/projetpoo/ProjetPOO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetpoo;
+
+import Vue.Fenetre;
+
+/**
+ *
+ * @author jjmar
+ */
+public class ProjetPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fenetre fen = new Fenetre();
+        fen.setVisible(true);
+            
+    }
+}
+   
